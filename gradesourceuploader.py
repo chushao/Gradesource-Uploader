c278852b96dea014201ef12b16435fd3c816bc2b
Chu Shao <cshao@churenshao.com>
2013-01-21 15:10:02 -0800

    added methods for GUI

--- gradesourceuploader.py
+++ gradesourceuploader.py
@@ -30,3 +30,24 @@ def updateScoresByEmail(login, courseID, assignmentID, CSVFile):
 def updateScoresByPID(login,courseID, assignmentID, CSVFile):
     gradesource = GradesourceSession(login, getpass('Password: '), courseID)
     gradesource.updatePIDScore(assignmentID, CSVFile)
+
+# FOR GUI. IF USED MANUALLY THEN PASSWORD IS PLAINTEXT. DO NOT USE THROUGH CLI.
+def downloadEmailGUI(login, courseID, password):
+    # Alternative:
+    # gradesource = GradesourceSession(self.staticLogin, getpass('Password: '), self.staticCourseID)
+    gradesource = GradesourceSession(login, password, courseID)
+    gradesource.downloadEmail()
+
+def downloadiClickerGUI(login, courseID, password):
+    gradesource = GradesourceSession(login, password, courseID)
+    gradesource.downloadiClicker()
+# To use: gradesourceuploader.updateScores(loginnname, courseID, assignment name, CSVFile)
+# Can also be replaced with static as well.
+#
+def updateScoresByEmailGUI(login, courseID, assignmentID, CSVFile, password):
+    gradesource = GradesourceSession(login, password, courseID)
+    gradesource.updateEmailScore(assignmentID, CSVFile)
+
+def updateScoresByPIDGUI(login,courseID, assignmentID, CSVFile, password):
+    gradesource = GradesourceSession(login, password, courseID)
+    gradesource.updatePIDScore(assignmentID, CSVFile)
